67232a9eabc0696fd84c422d618ab85486aa7532
Tayte Boss <boss.tayte@gmail.com>
2025-12-02 11:03:11 +0200

    style: Souvenir studio

--- frontend/styles/theme.tsx
+++ frontend/styles/theme.tsx
@@ -5,7 +5,7 @@ export const theme = {
     blue: "#000",
   },
   fonts: {
-    default: "sans-serif",
+    default: "Karrik Regular",
   },
   mediaBreakpoints: {
     desktop: "(min-width: 1680px)",
